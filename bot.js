cf2d4c415c3733ecfe004460310da940a3731a92
IAsapredevoluciones <papilarra@gmail.com>
2026-07-23 13:18:05 -0400

    Add files via upload

--- bot.js
+++ bot.js
@@ -33,7 +33,16 @@ app.listen(PORT, () => {
 const client = new Client({
     authStrategy: new LocalAuth(),
     puppeteer: {
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        args: [
+            '--no-sandbox', 
+            '--disable-setuid-sandbox', 
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
